e5053ceaa633c52815f34d597566679d49028095
patgpt <doitformurphy@gmail.com>
2025-01-08 03:09:32 -0500

    feat: add button row component with social media links and update localization files

---
+++ src/components/ButtonRow.tsx
@@ -0,0 +1,35 @@
+import Link from 'next/link';
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+import { FaGithub, FaLinkedin, FaTwitter } from 'react-icons/fa';
+
+const ButtonRow: React.FC = () => {
+  const { t } = useTranslation();
+
+  return (
+    <div className="flex space-x-4">
+      <Link href="https://github.com" aria-label={t('buttons.github')} className="btn btn-primary">
+        <FaGithub className="mr-2" />
+        {t('buttons.github')}
+      </Link>
+      <Link
+        href="https://linkedin.com"
+        aria-label={t('buttons.linkedin')}
+        className="btn btn-primary"
+      >
+        <FaLinkedin className="mr-2" />
+        {t('buttons.linkedin')}
+      </Link>
+      <Link
+        href="https://twitter.com"
+        aria-label={t('buttons.twitter')}
+        className="btn btn-primary"
+      >
+        <FaTwitter className="mr-2" />
+        {t('buttons.twitter')}
+      </Link>
+    </div>
+  );
+};
+
+export default ButtonRow;
