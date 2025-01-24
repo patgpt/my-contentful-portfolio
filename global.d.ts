023c40763d0a8b5055a065c8e7b7f540308c7eef
patgpt <doitformurphy@gmail.com>
2025-01-24 18:05:35 -0500

    feat(i18n): extend IntlMessages interface and add locale detection configuration

--- global.d.ts
+++ global.d.ts
@@ -2,8 +2,19 @@ import en from './messages/en.json';
 
 type Messages = typeof en;
 
+// Extend the `IntlMessages` interface with the message keys from the JSON files  
 declare global {
   // Use type safe message keys with `next-intl`
   // eslint-disable-next-line @typescript-eslint/no-empty-object-type
   interface IntlMessages extends Messages {}
+}
+
+// Next-intl configuration for locale detection
+import { routing } from '@/i18n/routing';
+
+declare module 'next-intl' {
+  interface AppConfig {
+    // ...
+    Locale: (typeof routing.locales)[number];
+  }
 }
